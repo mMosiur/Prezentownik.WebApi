c847f4683a1e1e1cb2045e7177a1d0d3482022d0
Mateusz Moruś <mpmorus@gmail.com>
2026-08-27 13:20:26 +0200

    Added file-based benchmarking class for quick benchmarks

---
+++ src/Prezentownik.Benchmarks.cs
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/env dotnet
+
+#:property PublishAot=false
+#:property Configuration=Release
+#:property Optimize=true
+
+#:package BenchmarkDotNet@0.15.8
+
+#pragma warning disable CA1822 Mark members as static
+#pragma warning disable CA1050 Declare types in namespaces
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+var summary = BenchmarkRunner.Run<Benchmarks>();
+
+
+[InProcess]
+public class Benchmarks
+{
+    [Benchmark]
+    public void Benchmark()
+    {
+    }
+}
